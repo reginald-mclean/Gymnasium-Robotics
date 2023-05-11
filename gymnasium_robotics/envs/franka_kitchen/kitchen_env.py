4e284d77f1f3fa8ae78e5b2e4e1257766f4ff106
Reginald McLean <reginald.mclean@ryerson.ca>
2023-05-11 11:23:12 -0400

    Added dense rewards for kettle

--- gymnasium_robotics/envs/franka_kitchen/kitchen_env.py
+++ gymnasium_robotics/envs/franka_kitchen/kitchen_env.py
@@ -25,6 +25,9 @@ from gymnasium_robotics.core import GoalEnv
 from gymnasium_robotics.envs.franka_kitchen.franka_env import FrankaRobot
 from gymnasium_robotics.utils.mujoco_utils import get_joint_qpos
 
+#from mujoco import mj_name2id, mjOBJ_BODY
+import mujoco
+
 OBS_ELEMENT_GOALS = {
     "bottom_right_burner": np.array([-0.01]),
     "bottom_left_burner": np.array([-0.01]),
@@ -330,6 +333,10 @@ class KitchenEnv(GoalEnv, EzPickle):
             )
         )
 
+        self.init_obj_pos = dict()
+        self.init_right_pad = dict()
+        self.init_left_pad = dict()
+        self.init_tcp = dict()
         EzPickle.__init__(
             self,
             tasks_to_complete,
@@ -345,13 +352,45 @@ class KitchenEnv(GoalEnv, EzPickle):
         desired_goal: "dict[str, np.ndarray]",
         info: "dict[str, Any]",
     ):
-        for task in info["tasks_to_complete"]:
-            distance = np.linalg.norm(achieved_goal[task] - desired_goal[task])
-            complete = distance < BONUS_THRESH
-            if complete:
-                self.step_task_completions.append(task)
 
-        return float(len(self.step_task_completions))
+
+        #print(self.data.body('hand'))
+        #print(self.data.body('right_finger'))
+        #print(self.data.body('left_finger'))
+        #print(self.goal.keys())
+        task = list(self.goal.keys())[0]
+        #print(task)
+        reward = 0.0
+        '''if task == 'kettle':
+            current_obj_pos = get_joint_qpos(self.model, self.data, task) # there's only one task here
+            
+            gripper_distance_apart = np.linalg.norm(self.data.body('right_finger').xpos - self.data.body('left_finger').xpos)
+            gripper_distance_apart = np.clip(gripper_distance_apart / 0.1, 0., 1.)
+            tcp_center = (self.data.body('right_finger').xpos + self.data.body('left_finger').xpos)/2.0
+            #print('calculating kettle reward')
+            target = self.goal[task]
+            if target.shape[0] != 7:
+                assert 1==2, 'need to handle smaller goal positions'
+            #print(target.shape[0])
+            obj_to_target = np.linalg.norm(current_obj_pos - target)
+            tcp_to_target = np.linalg.norm(current_obj_pos[:3] - tcp_center)
+            in_place_margin = (np.linalg.norm(self.init_obj_pos[task] - target))
+            tcp_center = np.concatenate([tcp_center, np.zeros(4)])
+            tcp_to_obj = np.linalg.norm(current_obj_pos - tcp_center)
+
+            assert len(info['tasks_to_complete']) == 1, "Cannot compute rewards like this for more than one task"
+            for task in info["tasks_to_complete"]:
+                in_place = tolerance(obj_to_target, bounds=(0, 0.05), margin=in_place_margin, sigmoid='long_tail')
+                object_grasped = self.gripper_caging_reward(np.zeros(4), current_obj_pos, task)
+                tcp_opened = gripper_distance_apart
+                obj = current_obj_pos
+                reward = hamacher_product(object_grasped, in_place)
+                if tcp_to_obj < 0.02 and (tcp_opened > 0) and (obj[2] - 0.01 > self.obj_init_pos[2]):
+                    reward += 1. + 5. * in_place
+                if obj_to_target < BONUS_THRESH:
+                    reward = 10.'''
+
+        return reward
 
     def _get_obs(self, robot_obs):
         obj_qpos = self.data.qpos[9:].copy()
@@ -408,7 +447,12 @@ class KitchenEnv(GoalEnv, EzPickle):
         robot_obs, _ = self.robot_env.reset(seed=seed)
         obs = self._get_obs(robot_obs)
         self.task_to_complete = self.goal.copy()
-
+        #task = list(self.goal.keys())[0]
+        #self.init_obj_pos[task] = get_joint_qpos(self.model, self.data, task)
+        #print(self.init_obj_pos)
+        #self.init_right_pad[task] = self.data.body('right_finger')
+        #self.init_left_pad[task] = self.data.body('left_finger')
+        #self.init_tcp[task] = (self.data.body('right_finger').xpos + self.data.body('left_finger').xpos)/2.0
         info = {
             "tasks_to_complete": self.task_to_complete,
             "episode_task_completions": [],
@@ -422,3 +466,192 @@ class KitchenEnv(GoalEnv, EzPickle):
 
     def close(self):
         self.robot_env.close()
+
+    def gripper_caging_reward(self, action, obj_position, task):
+        pad_success_margin = 0.05
+        x_z_success_margin = 0.005
+        obj_radius = 0.015
+        tcp = (self.data.body('right_finger').xpos + self.data.body('left_finger').xpos)/2.0
+        left_pad = self.data.body('left_finger').xpos
+        right_pad = self.data.body('right_finger').xpos
+        delta_object_y_left_pad = left_pad[1] - obj_position[1]
+        delta_object_y_right_pad = obj_position[1] - right_pad[1]
+        right_caging_margin = abs(abs(obj_position[1] - self.init_right_pad[task].xpos[1])
+            - pad_success_margin)
+        left_caging_margin = abs(abs(obj_position[1] - self.init_left_pad[task].xpos[1])
+            - pad_success_margin)
+        right_caging = tolerance(delta_object_y_right_pad,
+                            bounds=(obj_radius, pad_success_margin),
+                            margin=right_caging_margin,
+                            sigmoid='long_tail',)
+        left_caging = tolerance(delta_object_y_left_pad,
+                            bounds=(obj_radius, pad_success_margin),
+                            margin=left_caging_margin,
+                            sigmoid='long_tail',)
+        y_caging = hamacher_product(left_caging,
+                                                 right_caging)
+
+        # compute the tcp_obj distance in the x_z plane
+        tcp_xz = tcp + np.array([0., -tcp[1], 0.])
+        #print(tcp_xz)
+        #print('here')
+        tcp_xz = np.concatenate([tcp_xz, np.zeros(4)])
+        obj_position_x_z = np.copy(obj_position) + np.array([0., -obj_position[1], 0., 0., 0., 0., 0.])
+        tcp_obj_norm_x_z = np.linalg.norm(tcp_xz - obj_position_x_z, ord=2)
+
+        # used for computing the tcp to object object margin in the x_z plane
+        init_obj_x_z = self.init_obj_pos[task] + np.array([0., -self.init_obj_pos[task][1], 0., 0., 0., 0., 0.])
+        init_tcp_x_z = self.init_tcp[task] + np.array([0., -self.init_tcp[task][1], 0.])
+        init_tcp_x_z = np.concatenate([init_tcp_x_z, np.zeros(4)])
+        tcp_obj_x_z_margin = np.linalg.norm(init_obj_x_z - init_tcp_x_z, ord=2) - x_z_success_margin
+
+
+        x_z_caging = tolerance(tcp_obj_norm_x_z,
+                            bounds=(0, x_z_success_margin),
+                            margin=tcp_obj_x_z_margin,
+                            sigmoid='long_tail',)
+
+        gripper_closed = min(max(0, action[-1]), 1)
+        caging = hamacher_product(y_caging, x_z_caging)
+        gripping = gripper_closed if caging > 0.97 else 0.
+        caging_and_gripping = hamacher_product(caging, gripping)
+        caging_and_gripping = (caging_and_gripping + caging) / 2
+        return caging_and_gripping
+
+
+
+def tolerance(x,
+              bounds=(0.0, 0.0),
+              margin=0.0,
+              sigmoid='gaussian',
+              value_at_margin=0.1):
+    """Returns 1 when `x` falls inside the bounds, between 0 and 1 otherwise.
+
+    Args:
+        x: A scalar or numpy array.
+        bounds: A tuple of floats specifying inclusive `(lower, upper)` bounds for
+        the target interval. These can be infinite if the interval is unbounded
+        at one or both ends, or they can be equal to one another if the target
+        value is exact.
+        margin: Float. Parameter that controls how steeply the output decreases as
+        `x` moves out-of-bounds.
+        * If `margin == 0` then the output will be 0 for all values of `x`
+            outside of `bounds`.
+        * If `margin > 0` then the output will decrease sigmoidally with
+            increasing distance from the nearest bound.
+        sigmoid: String, choice of sigmoid type. Valid values are: 'gaussian',
+        'linear', 'hyperbolic', 'long_tail', 'cosine', 'tanh_squared'.
+        value_at_margin: A float between 0 and 1 specifying the output value when
+        the distance from `x` to the nearest bound is equal to `margin`. Ignored
+        if `margin == 0`.
+
+    Returns:
+        A float or numpy array with values between 0.0 and 1.0.
+
+    Raises:
+        ValueError: If `bounds[0] > bounds[1]`.
+        ValueError: If `margin` is negative.
+    """
+    lower, upper = bounds
+    if lower > upper:
+        raise ValueError('Lower bound must be <= upper bound.')
+    if margin < 0:
+        raise ValueError('`margin` must be non-negative. Current value: {}'.format(margin))
+
+    in_bounds = np.logical_and(lower <= x, x <= upper)
+    if margin == 0:
+        value = np.where(in_bounds, 1.0, 0.0)
+    else:
+        d = np.where(x < lower, lower - x, x - upper) / margin
+        value = np.where(in_bounds, 1.0, _sigmoids(d, value_at_margin,
+                                                   sigmoid))
+
+    return float(value) if np.isscalar(x) else value
+
+def _sigmoids(x, value_at_1, sigmoid):
+    """Returns 1 when `x` == 0, between 0 and 1 otherwise.
+
+    Args:
+        x: A scalar or numpy array.
+        value_at_1: A float between 0 and 1 specifying the output when `x` == 1.
+        sigmoid: String, choice of sigmoid type.
+
+    Returns:
+        A numpy array with values between 0.0 and 1.0.
+
+    Raises:
+        ValueError: If not 0 < `value_at_1` < 1, except for `linear`, `cosine` and
+        `quadratic` sigmoids which allow `value_at_1` == 0.
+        ValueError: If `sigmoid` is of an unknown type.
+    """
+    if sigmoid in ('cosine', 'linear', 'quadratic'):
+        if not 0 <= value_at_1 < 1:
+            raise ValueError(
+                '`value_at_1` must be nonnegative and smaller than 1, '
+                'got {}.'.format(value_at_1))
+    else:
+        if not 0 < value_at_1 < 1:
+            raise ValueError('`value_at_1` must be strictly between 0 and 1, '
+                             'got {}.'.format(value_at_1))
+
+    if sigmoid == 'gaussian':
+        scale = np.sqrt(-2 * np.log(value_at_1))
+        return np.exp(-0.5 * (x * scale)**2)
+
+    elif sigmoid == 'hyperbolic':
+        scale = np.arccosh(1 / value_at_1)
+        return 1 / np.cosh(x * scale)
+
+    elif sigmoid == 'long_tail':
+        scale = np.sqrt(1 / value_at_1 - 1)
+        return 1 / ((x * scale)**2 + 1)
+
+    elif sigmoid == 'reciprocal':
+        scale = 1 / value_at_1 - 1
+        return 1 / (abs(x) * scale + 1)
+
+    elif sigmoid == 'cosine':
+        scale = np.arccos(2 * value_at_1 - 1) / np.pi
+        scaled_x = x * scale
+        return np.where(
+            abs(scaled_x) < 1, (1 + np.cos(np.pi * scaled_x)) / 2, 0.0)
+
+    elif sigmoid == 'linear':
+        scale = 1 - value_at_1
+        scaled_x = x * scale
+        return np.where(abs(scaled_x) < 1, 1 - scaled_x, 0.0)
+
+    elif sigmoid == 'quadratic':
+        scale = np.sqrt(1 - value_at_1)
+        scaled_x = x * scale
+        return np.where(abs(scaled_x) < 1, 1 - scaled_x**2, 0.0)
+
+    elif sigmoid == 'tanh_squared':
+        scale = np.arctanh(np.sqrt(1 - value_at_1))
+        return 1 - np.tanh(x * scale)**2
+
+    else:
+        raise ValueError('Unknown sigmoid type {!r}.'.format(sigmoid))
+
+def hamacher_product(a, b):
+    """The hamacher (t-norm) product of a and b.
+
+    computes (a * b) / ((a + b) - (a * b))
+
+    Args:
+        a (float): 1st term of hamacher product.
+        b (float): 2nd term of hamacher product.
+    Raises:
+        ValueError: a and b must range between 0 and 1
+
+    Returns:
+        float: The hammacher product of a and b
+    """
+    if not ((0. <= a <= 1.) and (0. <= b <= 1.)):
+        raise ValueError("a and b must range between 0 and 1")
+
+    denominator = a + b - (a * b)
+    h_prod = ((a * b) / denominator) if denominator > 0 else 0
+
+    assert 0. <= h_prod <= 1.
+    return h_prod
